2a434eefd3b9996c83101a02a6921f2b48570775
sanko <sang.koh@hotmail.com>
2023-05-02 19:45:01 +1000

    spring boot

---
+++ src/main/java/sanko/sankons/SankonsApplication.java
@@ -0,0 +1,13 @@
+package sanko.sankons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SankonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SankonsApplication.class, args);
+	}
+
+}
