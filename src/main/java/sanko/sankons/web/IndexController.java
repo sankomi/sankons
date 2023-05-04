9cb967edc6a72b6d81458d6cc9b267c5464a163c
sanko <sang.koh@hotmail.com>
2023-05-04 21:14:09 +1000

    redis session

---
+++ src/main/java/sanko/sankons/web/IndexController.java
@@ -0,0 +1,21 @@
+package sanko.sankons.web;
+
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.*; //RestController, GetMapping
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+public class IndexController {
+
+	private final HttpSession httpSession;
+
+	@GetMapping("/")
+	public String index() {
+		httpSession.setAttribute("test", String.valueOf(Math.random()));
+
+		return (String) httpSession.getAttribute("test");
+	}
+
+}
