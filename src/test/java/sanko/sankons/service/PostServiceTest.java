4f214a2f6d722faa371f1a4bed70f3bf3e4adbec
sanko <sang.koh@hotmail.com>
2023-06-01 21:56:21 +1000

    post test

--- src/test/java/sanko/sankons/service/PostServiceTest.java
+++ src/test/java/sanko/sankons/service/PostServiceTest.java
@@ -1,6 +1,7 @@
 package sanko.sankons.service;
 
-import java.util.Optional;
+import java.util.*; //List, ArrayList, Optional;
+import java.util.stream.Collectors;
 import jakarta.servlet.http.HttpSession;
 
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.data.domain.*; //Page, Pageable
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -21,9 +23,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import sanko.sankons.domain.user.User;
 import sanko.sankons.domain.user.UserRepository;
 import sanko.sankons.domain.post.Post;
-import sanko.sankons.domain.post.PostRepository;
+import sanko.sankons.domain.post.*; //PostRepository, PostPagingRepository
 import sanko.sankons.web.dto.SessionUser;
-import sanko.sankons.web.dto.*; //PostPostRequest, PostListRequest, PostViewResponse
+import sanko.sankons.web.dto.*; //PostPostRequest, PostListRequest, PostViewResponse, PostListResponse
 
 @ExtendWith(SpringExtension.class)
 @Import(PostService.class)
@@ -39,14 +41,23 @@ public class PostServiceTest {
 	private PostRepository postRepository;
 
 	@MockBean
+	private PostPagingRepository postPagingRepository;
+
+	@MockBean
 	private HttpSession httpSession;
 
 	private static final Long userId = 1L;
 	private static final String username = "username";
 	private static final String password = "password";
+
 	private static final Long postId = 2L;
 	private static final String image = "image";
 	private static final String content = "content";
+
+	private static final int page = 0;
+	private static final int size = 2;
+
+	private static final List<Post> posts = new ArrayList<>();
 
 	private static User user;
 	private static Post post;
@@ -64,10 +75,20 @@ public class PostServiceTest {
 			.image(image)
 			.content(content)
 			.build();
+
+		for (int i = 0; i < 5; i++) {
+			Post post = Post.builder()
+				.poster(user)
+				.image(image)
+				.content(content)
+				.build();
+			ReflectionTestUtils.setField(post, "id", Long.valueOf(i));
+			posts.add(post);
+		}
 	}
 
 	@BeforeEach
-	public void beforeEach() {
+	public void mockRepositorys() {
 		when(userRepository.findById(userId))
 			.thenReturn(Optional.of(user));
 
@@ -82,6 +103,16 @@ public class PostServiceTest {
 
 		when(postRepository.findById(postId))
 			.thenReturn(Optional.of(post));
+
+		Page<Post> postPage = mock(Page.class);
+		when(postPage.getContent()).thenReturn(
+			posts.stream()
+				.limit(size)
+				.collect(Collectors.toList())
+
+		);
+		when(postPagingRepository.findAll(any(Pageable.class)))
+			.thenReturn(postPage);
 	}
 
 	@Test
@@ -110,6 +141,22 @@ public class PostServiceTest {
 		assertEquals(content, view.getContent());
 		assertEquals(userId, view.getPoster().getId());
 		assertEquals(username, view.getPoster().getUsername());
+	}
+
+	@Test
+	public void testPostList() throws Exception {
+		//given
+		PostListRequest request = new PostListRequest(page, size);
+
+		//when
+		PostListResponse response = postService.list(request);
+
+		//then
+		assertEquals(page, response.getPage());
+		assertEquals(size, response.getSize());
+		assertEquals(size, response.getPosts().size());
+		assertEquals(content, response.getPosts().get(0).getContent());
+		assertEquals(username, response.getPosts().get(0).getPoster().getUsername());
 	}
 
 }
--- src/test/java/sanko/sankons/web/PostApiControllerTest.java
+++ src/test/java/sanko/sankons/web/PostApiControllerTest.java
@@ -1,5 +1,7 @@
 package sanko.sankons.web;
 
+import java.util.*; //List, ArrayList;
+
 import org.junit.jupiter.api.*; //Test, BeforeAll, BeforeEach
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,7 +26,7 @@ import static org.hamcrest.Matchers.is;
 import sanko.sankons.domain.user.User;
 import sanko.sankons.domain.post.Post;
 import sanko.sankons.service.PostService;
-import sanko.sankons.web.dto.*; //PostPostRequest, PostViewResponse
+import sanko.sankons.web.dto.*; //PostPostRequest, PostViewResponse, PostListRequest, PostViewResponse
 
 @WebMvcTest(PostApiController.class)
 public class PostApiControllerTest {
@@ -38,9 +40,15 @@ public class PostApiControllerTest {
 	private static final Long userId = 1L;
 	private static final String username = "username";
 	private static final String password = "password";
+
 	private static final Long postId = 1L;
 	private static final String image = "image.jpg";
 	private static final String content = "content";
+
+	private static final List<Post> posts = new ArrayList();
+
+	private static final int page = 0;
+	private static final int size = 2;
 
 	private static User user;
 	private static Post post;
@@ -59,12 +67,32 @@ public class PostApiControllerTest {
 			.content(content)
 			.build();
 		ReflectionTestUtils.setField(post, "id", postId);
+
+		for (int i = 0; i < size; i++) {
+			Post post = Post.builder()
+				.poster(user)
+				.image(image)
+				.content(content)
+				.build();
+			ReflectionTestUtils.setField(post, "id", Long.valueOf(i));
+			posts.add(post);
+		}
 	}
 
 	@BeforeEach
 	public void mockPostService() throws Exception {
 		when(postService.post(any(PostPostRequest.class), any(MultipartFile.class)))
 			.thenReturn(postId);
+
+		when(postService.list(any(PostListRequest.class)))
+			.thenAnswer(invocation -> {
+				PostListRequest request = invocation.getArgument(0, PostListRequest.class);
+				return PostListResponse.builder()
+					.page(request.getPage())
+					.size(request.getSize())
+					.posts(posts)
+					.build();
+			});
 
 		when(postService.view(postId)).thenReturn(new PostViewResponse(post));
 	}
@@ -93,6 +121,15 @@ public class PostApiControllerTest {
 		)
 			.andExpect(status().isOk())
 			.andExpect(content().string(postId.toString()));
+	}
+
+	@Test
+	public void testPostList() throws Exception {
+		mockMvc.perform(get("/api/v1/post/list?page=0&size=2"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.posts[0].id", is(0)))
+			.andExpect(jsonPath("$.posts[0].content", is(content)))
+			.andExpect(jsonPath("$.posts[0].poster.username", is(username)));
 	}
 
 	@Test
